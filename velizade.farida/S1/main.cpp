67e7bc8866d7fd7cc66d04e111c8eafe64b08189
Farida <velizadefarida@gmail.com>
2026-04-18 04:32:38 +0300

    nadeyus poluchitsya

--- velizade.farida/S1/main.cpp
+++ velizade.farida/S1/main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <utility>
 #include <sstream>
+#include <limits>
 
 using namespace velizade;
 
@@ -10,23 +11,22 @@ int main()
 {
   try
   {
-    List<std::pair<std::string, List<int>>> sequences;
-
+    List<std::pair<std::string, List<unsigned long long>>> sequences;
     std::string line;
+
     while (std::getline(std::cin, line))
     {
-      if (line.empty())
+      if (line.empty()) {
         continue;
-
+      }
       std::istringstream iss(line);
       std::string name;
       iss >> name;
-
-      List<int> numbers;
-      int value;
-      while (iss >> value)
+      List<unsigned long long> numbers;
+      unsigned long long num;
+      while (iss >> num)
       {
-        numbers.push_back(value);
+        numbers.push_back(num);
       }
       sequences.push_back({name, std::move(numbers)});
     }
@@ -40,60 +40,77 @@ int main()
     auto nameIt = sequences.cbegin();
     std::cout << nameIt->first;
     ++nameIt;
-    for (; nameIt != sequences.cend(); ++nameIt)
+    for (; nameIt != sequences.cend(); ++nameIt) {
       std::cout << ' ' << nameIt->first;
+    }
     std::cout << '\n';
 
     size_t maxLen = 0;
-    for (auto it = sequences.cbegin(); it != sequences.cend(); ++it)
-      if (it->second.size() > maxLen)
-        maxLen = it->second.size();
-
+    for (auto seq = sequences.cbegin(); seq != sequences.cend(); ++seq) {
+      if (seq->second.size() > maxLen) {
+        maxLen = seq->second.size();
+      }
+    }
     if (maxLen == 0)
     {
-      std::cerr << "Error: cannot calculate sums\n";
-      return 1;
+      std::cout << '\n';
+      return 0;
     }
 
-    List<List<int>> columns;
-    for (size_t col = 0; col < maxLen; ++col)
+    List<List<unsigned long long>> columns;
+    for (size_t i = 0; i < maxLen; ++i)
     {
-      List<int> column;
+      List<unsigned long long> col;
       for (auto seq = sequences.cbegin(); seq != sequences.cend(); ++seq)
       {
-        if (col < seq->second.size())
+        if (i < seq->second.size())
         {
           auto elem = seq->second.cbegin();
-          for (size_t k = 0; k < col; ++k)
+          for (size_t j = 0; j < i; ++j) {
             ++elem;
-          column.push_back(*elem);
+          }
+          col.push_back(*elem);
         }
       }
-      columns.push_back(std::move(column));
+      columns.push_back(std::move(col));
     }
 
-    List<long long> sums;
+    List<unsigned long long> sums;
+    bool overflow = false;
     for (auto col = columns.cbegin(); col != columns.cend(); ++col)
     {
       bool firstElem = true;
-      long long colSum = 0;
+      unsigned long long colSum = 0;
       for (auto val = col->cbegin(); val != col->cend(); ++val)
       {
-        if (!firstElem)
+        if (!firstElem) {
           std::cout << ' ';
+        }
         std::cout << *val;
         firstElem = false;
-        colSum += *val;
+        if (colSum > std::numeric_limits<unsigned long long>::max() - *val) {
+          overflow = true;
+        }
+        else {
+          colSum += *val;
+        }
       }
       std::cout << '\n';
       sums.push_back(colSum);
     }
 
+    if (overflow)
+    {
+      std::cerr << "Error: overflow\n";
+      return 1;
+    }
+
     auto sumIt = sums.cbegin();
     std::cout << *sumIt;
     ++sumIt;
-    for (; sumIt != sums.cend(); ++sumIt)
+    for (; sumIt != sums.cend(); ++sumIt) {
       std::cout << ' ' << *sumIt;
+    }
     std::cout << '\n';
 
     return 0;
