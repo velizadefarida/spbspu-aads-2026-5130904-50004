c3eb4d1badf780b7059d660bd484a1a251815ce8
Farida <velizadefarida@gmail.com>
2026-06-20 21:01:31 +0300

    add graph.hpp

---
+++ velizade.farida/S3/graph.hpp
@@ -0,0 +1,42 @@
+#ifndef GRAPH_HPP
+#define GRAPH_HPP
+
+#include <string>
+#include <utility>
+#include <functional>
+#include "hash_table.hpp"
+#include "vector.hpp"
+
+namespace velizade
+{
+
+using EdgeKey = std::pair<std::string, std::string>;
+using EdgeTable = HashTable<EdgeKey, Vector<int>>;
+
+struct Graph
+{
+    std::string name;
+    Vector<std::string> vertices;
+    EdgeTable edges;
+
+    Graph();
+    explicit Graph(const std::string& n);
+
+    void addVertex(const std::string& v);
+    void addEdge(const std::string& from, const std::string& to, int weight);
+    bool removeEdge(const std::string& from, const std::string& to, int weight);
+    bool hasVertex(const std::string& v) const;
+};
+
+using GraphTable = HashTable<std::string, Graph>;
+GraphTable& getGraphs();
+
+void loadGraphsFromFile(const std::string& filename);
+
+Vector<std::string> splitLine(const std::string& line);
+void sortStrings(Vector<std::string>& vec);
+void printLines(const Vector<std::string>& lines);
+
+}
+
+#endif
