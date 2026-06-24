0e204426a10f1e16fe8b25ab5d8595cbe41aa82e
Farida <velizadefarida@gmail.com>
2026-06-24 19:42:46 +0300

    add status.hpp

---
+++ velizade.farida/F0/status.hpp
@@ -0,0 +1,20 @@
+#ifndef STATUS_HPP
+#define STATUS_HPP
+
+#include <string>
+
+namespace velizade {
+
+  enum class Status
+  {
+    UNREAD,
+    READING,
+    FINISHED
+  };
+
+  std::string statusToString(Status s);
+  Status stringToStatus(const std::string& str);
+
+}
+
+#endif
