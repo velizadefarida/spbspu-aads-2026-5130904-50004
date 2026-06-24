feb12f4252134ddf5d6afd4b6afed19f03bcc498
Farida <velizadefarida@gmail.com>
2026-06-24 19:38:28 +0300

    add book.hpp

---
+++ velizade.farida/F0/book.hpp
@@ -0,0 +1,17 @@
+#ifndef BOOK_HPP
+#define BOOK_HPP
+
+#include <string>
+
+namespace velizade {
+
+  struct Book
+  {
+    std::string author;
+    std::string title;
+    int year;
+  };
+
+}
+
+#endif
